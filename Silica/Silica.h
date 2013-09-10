05cd8ca5737365d30ee882c57a13c015134589de
Steven Degutis <steven@cleancoders.com>
2013-09-10 09:45:22 -0500

    Adding Xcode Framework project

---
+++ Silica/Silica.h
@@ -0,0 +1,13 @@
+//
+//  Silica.h
+//  Silica
+//
+//  Created by Steven on 9/10/13.
+//  Copyright (c) 2013 SiO2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Silica : NSObject
+
+@end
---
+++ Silica/Silica.m
@@ -0,0 +1,13 @@
+//
+//  Silica.m
+//  Silica
+//
+//  Created by Steven on 9/10/13.
+//  Copyright (c) 2013 SiO2. All rights reserved.
+//
+
+#import "Silica.h"
+
+@implementation Silica
+
+@end
