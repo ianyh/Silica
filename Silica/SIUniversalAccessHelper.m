04a6904d3f3387071bf8fe1b07d6d6c66587f61b
Ian Ynda-Hummel <ianynda@gmail.com>
2014-04-06 10:53:00 -0400

    Don't use Zephyros application name on SIUniversalAccessHelper's complain alert

--- Silica/SIUniversalAccessHelper.m
+++ Silica/SIUniversalAccessHelper.m
@@ -21,15 +21,17 @@
 
         NSString *applicationName = NSBundle.mainBundle.infoDictionary[(__bridge NSString *)kCFBundleNameKey];
         NSString *alertTitle = [NSString stringWithFormat:@"%@ Requires Universal Access", applicationName];
+        NSString *firstLine = [NSString stringWithFormat:@"For %@ to function properly, access for assistive devices must be enabled first.\n\n", applicationName];
+
         NSInteger result = NSRunAlertPanel(alertTitle,
-                                           @"For Zephyros to function properly, access for assistive devices must be enabled first.\n\n"
+                                           firstLine,
                                            @"To enable this feature, click \"Enable access for assistive devices\" in the Universal Access pane of System Preferences.",
                                            @"Open Universal Access",
                                            @"Dismiss",
                                            nil);
         
         if (result == NSAlertDefaultReturn) {
-            NSString* src = @"tell application \"System Preferences\"\nactivate\nset current pane to pane \"com.apple.preference.universalaccess\"\nend tell";
+            NSString *src = @"tell application \"System Preferences\"\nactivate\nset current pane to pane \"com.apple.preference.universalaccess\"\nend tell";
             NSAppleScript *a = [[NSAppleScript alloc] initWithSource:src];
             [a executeAndReturnError:nil];
         }
